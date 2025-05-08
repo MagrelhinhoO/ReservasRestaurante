f6e4ebf9dbdade9edde3bb64180acabf03b9d5d8
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-05-08 19:04:04 -0300

    Adição do MainApp

--- src/main/java/com/restaurante/MainApp.java
+++ src/main/java/com/restaurante/MainApp.java
@@ -1,4 +1,10 @@
 package com.restaurante;
 
+import com.restaurante.controller.RestauranteController;
+
 public class MainApp {
+    public static void main(String[] args) {
+        RestauranteController controller = new RestauranteController();
+        controller.iniciar();
+    }
 }
